55de709dea4bb736b4c73e4d77db099ac4e6a5bc
Lucas <limalucas7027@gmail.com>
2023-11-17 13:46:47 -0300

    Added a listener to back and next browser buttons

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+window.onhashchange = () => {
+    hashRequested = window.location.hash.replace("#", "")
+    loadPage(hashRequested)
+    
+    }
+    
+
+
 function loadPage(page) {
     document.querySelectorAll('.container').forEach(element => {
         element.classList.remove('show')
     })
     document.querySelector(`#${page}`).classList.add('show')
+    window.location.hash = `#${page}`
+    
 }
 
 document.querySelectorAll('button').forEach(button => {
-    button.addEventListener('click', () => {loadPage(button.dataset.page)})
+    button.addEventListener('click', () => {
+        loadPage(button.dataset.page)
+    })
 })
