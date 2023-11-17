bed06d03986895e88871b765d4048e1bbcd9627b
Lucas <limalucas7027@gmail.com>
2023-11-17 14:16:02 -0300

    Second solution to make browser buttons work

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
-window.onhashchange = () => {
-    hashRequested = window.location.hash.replace("#", "")
-    loadPage(hashRequested)
-    
-    }
-    
-
-
+window.onpopstate = function(event) {
+    loadPage(event.state.page)
+}   
 function loadPage(page) {
     document.querySelectorAll('.container').forEach(element => {
         element.classList.remove('show')
     })
     document.querySelector(`#${page}`).classList.add('show')
-    window.location.hash = `#${page}`
-    
 }
 
 document.querySelectorAll('button').forEach(button => {
-    button.addEventListener('click', () => {
-        loadPage(button.dataset.page)
-    })
-})
+    button.onclick = function(){
+        let page = this.dataset.page;
+        history.pushState({"page": page}, "", `#${page}`);
+        loadPage(button.dataset.page);
+    }})
